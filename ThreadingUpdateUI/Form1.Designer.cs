7326e37030fa5ee40289db6df7ed07f1463c1674
CROESUZZ <k.gabrielcr@hotmail.de>
2023-04-26 12:15:30 -0300

    Added threading demo

--- ThreadingUpdateUI/Form1.Designer.cs
+++ ThreadingUpdateUI/Form1.Designer.cs
@@ -40,9 +40,10 @@
             // 
             // btnGo
             // 
-            this.btnGo.Location = new System.Drawing.Point(15, 153);
+            this.btnGo.Location = new System.Drawing.Point(18, 177);
+            this.btnGo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnGo.Name = "btnGo";
-            this.btnGo.Size = new System.Drawing.Size(312, 23);
+            this.btnGo.Size = new System.Drawing.Size(364, 27);
             this.btnGo.TabIndex = 0;
             this.btnGo.Text = "Go";
             this.btnGo.UseVisualStyleBackColor = true;
@@ -50,48 +51,54 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(126, 12);
+            this.textBox1.Location = new System.Drawing.Point(147, 14);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(204, 20);
+            this.textBox1.Size = new System.Drawing.Size(237, 23);
             this.textBox1.TabIndex = 2;
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(15, 182);
+            this.panel1.Location = new System.Drawing.Point(18, 210);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(315, 402);
+            this.panel1.Size = new System.Drawing.Size(368, 464);
             this.panel1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(126, 38);
+            this.textBox2.Location = new System.Drawing.Point(147, 44);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(204, 20);
+            this.textBox2.Size = new System.Drawing.Size(237, 23);
             this.textBox2.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Location = new System.Drawing.Point(18, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.Size = new System.Drawing.Size(57, 15);
             this.label1.TabIndex = 5;
             this.label1.Text = "Count to:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 41);
+            this.label2.Location = new System.Drawing.Point(18, 47);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.Size = new System.Drawing.Size(114, 15);
             this.label2.TabIndex = 6;
             this.label2.Text = "Sleep betwen count:";
             // 
             // btnGoNoThread
             // 
-            this.btnGoNoThread.Location = new System.Drawing.Point(12, 85);
+            this.btnGoNoThread.Location = new System.Drawing.Point(14, 98);
+            this.btnGoNoThread.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnGoNoThread.Name = "btnGoNoThread";
-            this.btnGoNoThread.Size = new System.Drawing.Size(312, 23);
+            this.btnGoNoThread.Size = new System.Drawing.Size(364, 27);
             this.btnGoNoThread.TabIndex = 7;
             this.btnGoNoThread.Text = "Go No Thread";
             this.btnGoNoThread.UseVisualStyleBackColor = true;
@@ -100,17 +107,18 @@
             // lblCounter
             // 
             this.lblCounter.AutoSize = true;
-            this.lblCounter.Location = new System.Drawing.Point(123, 111);
+            this.lblCounter.Location = new System.Drawing.Point(144, 128);
+            this.lblCounter.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCounter.Name = "lblCounter";
-            this.lblCounter.Size = new System.Drawing.Size(85, 13);
+            this.lblCounter.Size = new System.Drawing.Size(89, 15);
             this.lblCounter.TabIndex = 8;
             this.lblCounter.Text = "[MY COUNTER]";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(338, 596);
+            this.ClientSize = new System.Drawing.Size(394, 688);
             this.Controls.Add(this.lblCounter);
             this.Controls.Add(this.btnGoNoThread);
             this.Controls.Add(this.label2);
@@ -119,9 +127,11 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnGo);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- ThreadingUpdateUI/Form1.cs
+++ ThreadingUpdateUI/Form1.cs
@@ -18,21 +18,74 @@ namespace ThreadingUpdateUI
             InitializeComponent();
         }
 
+        private List<Label> threadlables = new List<Label>();
       
         private void btnGo_Click(object sender, EventArgs e)
         {
+            if(threadlables.Count < 100) 
+            {
+                //Reading from UI
+                int.TryParse(textBox1.Text, out int iteration);
+                int.TryParse(textBox2.Text, out int sleepDuration);
 
+                // Setting label
+                Label l = new Label();
+                l.Text = DateTime.Now.ToLongTimeString();
+                threadlables.Add(l);
+
+                l.Top = (panel1.Controls.Count) * l.Height + 20;
+                l.Left = 5;
+
+                //Adding label we created so it's actually ont he UI
+                this.panel1.Controls.Add(l);
+
+                //Creating thread
+                Task.Factory.StartNew(() => 
+                {
+                    //doing some stuff in the thread
+                    for (int i = 0; i <= iteration; i++)
+                    {
+                        this.Invoke((MethodInvoker)delegate 
+                        {
+                            l.Text = i.ToString();
+                        });
+
+                        Thread.Sleep(sleepDuration);
+                    }
+                });
+            }
         }
 
         private void btnGoNoThread_Click(object sender, EventArgs e)
         {
+            int.TryParse(textBox1.Text, out int iteration);
+            int.TryParse(textBox2.Text, out int sleepDuration);
 
+            btnGoNoThread.Enabled= false;
+            textBox1.Enabled= false;
+            textBox2.Enabled= false;
+
+            for (int i = 0; i <= iteration; i++)
+            {
+                lblCounter.Text = i.ToString();
+                lblCounter.Refresh();
+                Thread.Sleep(sleepDuration);
+            }
+
+            btnGoNoThread.Enabled = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             //We need to kill any running threads
-            
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
